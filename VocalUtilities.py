26c1626c997c3b1fa5f9ab1e968ed51985a7cb6e
ttgc <ttgc@users.noreply.github.com>
2017-11-20 18:49:05 +0100

    Upgrading vocal tools

--- VocalUtilities.py
+++ VocalUtilities.py
@@ -2,6 +2,22 @@
 #-*-coding:utf-8-*-
 #Vocal Utilities for Turtle Bot
 
+##    TtgcBot - a bot for discord
+##    Copyright (C) 2017  Thomas PIOT
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program. If not, see <http://www.gnu.org/licenses/>
+
 from threading import Thread
 import time
 import asyncio
@@ -17,17 +33,55 @@ def singleton(classe_definie):
     return get_instance
 
 @singleton
-class VocalSystem:
+class VocalCore:
     def __init__(self):
+        self.bot = None
+        self.loop = asyncio.get_event_loop()
+        self.voclist = {}
+
+    def _addtolist(self,servid,vocobj):
+        if not servid in self.voclist:
+            self.voclist[servid] = vocobj
+            return True
+        return False
+
+    def removefromlist(self,servid):
+        if servid in self.voclist:
+            vocobj = self.voclist[servid]
+            del(vocobj)
+            del(self.voclist[servid])
+            return True
+        return False
+
+    def getvocal(self,servid):
+        if servid in self.voclist:
+            return self.voclist[servid]
+        return None
+
+    @asyncio.coroutine
+    def interupt(self):
+        for i in self.voclist.keys():
+            if self.voclist[i].vocal:
+                yield from self.bot.send_message(self.voclist[i].textchan,":x: System interuption launched by administrators, vocal disconnected")
+                yield from self.voclist[i].leave()
+            vocobj = self.voclist[i]
+            del(vocobj)
+            self.voclist[i] = None
+        self.voclist = {}
+                
+        
+class VocalSystem:
+    def __init__(self,servid,vc=VocalCore()):
         self.vocal = False
         self.co = None
-        self.loop = asyncio.get_event_loop()
+        self.loop = vc.loop
         self.timer = VocalTimeout(60,self.loop)
         self.queue = []
         self.current = None
         self.is_playing = False
         self.textchan = None
-        self.bot = None
+        self.bot = vc.bot
+        vc._addtolist(servid,self)
 
     @asyncio.coroutine
     def join(self,chan,textchan):

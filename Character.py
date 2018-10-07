f0fb4e2e5fd1dc4c95357957a990b70ba15683c1
ttgc <ttgc@users.noreply.github.com>
2018-10-07 19:04:42 +0200

    Adding Pet class and links to database
    Adding Pet class
    Adding link methods to read/write database for pets features
    Missing roll for pets

--- Character.py
+++ Character.py
@@ -140,7 +140,7 @@ class Inventory:
 
 class Character:
     """Character class"""
-    def __init__(self,dic={"charkey":"","name":"","lore":"","PVm":1,"PMm":1,"force":50,"esprit":50,"charisme":50,"furtivite":50,"karma":0,"money":0,"stat":[0,0,0,0,0,0,0],"lp":0,"dp":0,"regenkarm":0.1,"mod":0,"armor":0,"RM":0,"PV":1,"PM":1,"default_mod":0,"default_karma":0,"intuition":3,"mentalhealth":100,"lvl":1,"linked":None,"inventory":Inventory()}):
+    def __init__(self,dic={"charkey":"","name":"","lore":"","PVm":1,"PMm":1,"force":50,"esprit":50,"charisme":50,"furtivite":50,"karma":0,"money":0,"stat":[0,0,0,0,0,0,0],"lp":0,"dp":0,"regenkarm":0.1,"mod":0,"armor":0,"RM":0,"PV":1,"PM":1,"default_mod":0,"default_karma":0,"intuition":3,"mentalhealth":100,"lvl":1,"linked":None,"inventory":Inventory(),"pet":{}}):
         self.key = dic["charkey"]
         self.name = dic["name"]
         self.lore = dic["lore"]
@@ -167,6 +167,7 @@ class Character:
         self.linked = dic["linked"]
         if self.linked.upper() == "NULL": self.linked = None
         self.inventory = dic["inventory"]
+        self.pet = dic["pet"]
         self.jdr = None
         #mod 0 = offensiv / mod 1 = defensiv
 
@@ -176,6 +177,8 @@ class Character:
     def bind(self,jdr):
         self.inventory.bind(self,jdr)
         self.jdr = jdr
+        for i in self.pet.keys():
+            self.pet[i].bind(jdr)
 
     def check_life(self):
         if self.PV <= 0:
@@ -632,3 +635,85 @@ class Character:
             yield from client.send_message(channel,lang["result_test"].format(lang[stat],str(result),str(self.intuition+modifier)))
         if self.karma > 10: self.karma = 10
         if self.karma < -10: self.karma = -10
+
+    def pet_add(self,key):
+        if key in self.pet: return
+        db = Database()
+        db.call("petcreate",dbkey=key,charact=self.key,idserv=self.jdr.server,idchan=self.jdr.channel)
+        db.close()
+        newpet = Pet()
+        newpet.key = newpet.name = key
+        newpet.charkey = self.key
+        self.pet[key] = newpet
+
+    def pet_delete(self,key):
+        if key not in self.pet: return
+        db = Database()
+        db.call("petdelete",dbkey=key,charact=self.key,idserv=self.jdr.server,idchan=self.jdr.channel)
+        db.close()
+        del(self.pet[key])
+
+class Pet:
+    def __init__(self,dic={"petkey":"","charkey":"","name":"","espece":"Unknown","PVm":1,"PMm":0,"force":50,"esprit":50,"charisme":50,"agilite":50,"karma":0,"stat":[0,0,0,0,0,0,0],"mod":0,"PV":1,"PM":0,"default_mod":0,"instinct":3,"lvl":1}):
+        self.key = dic["petkey"]
+        self.charkey = dic["charkey"]
+        self.name = dic["name"]
+        self.espece = dic["espece"]
+        self.PVmax = dic["PVm"]
+        self.PMmax = dic["PMm"]
+        self.PV = dic["PV"]
+        self.PM = dic["PM"]
+        self.force = dic["force"]
+        self.esprit = dic["esprit"]
+        self.charisme = dic["charisme"]
+        self.agilite = dic["agilite"]
+        self.karma = dic["karma"]
+        self.stat = dic["stat"]
+        self.mod = dic["mod"]
+        self.default_mod = dic["default_mod"]
+        self.instinct = dic["instinct"]
+        self.lvl = dic["lvl"]
+        self.jdr = None
+
+    def __str__(self):
+        return self.name
+
+    def bind(self,jdr):
+        self.jdr = jdr
+
+    def check_life(self):
+        return self.PV > 0
+
+    def petset(self,tag,value):
+        db = Database()
+        db.call("petset",dbkey=self.key,charact=self.charkey,idserv=self.jdr.server,idchan=self.jdr.channel,stat=tag,val=value)
+        db.close()
+        return self.jdr.get_character(self.charkey)
+
+    def setname(self,name_):
+        db = Database()
+        db.call("petsetname",dbkey=self.key,charact=self.charkey,idserv=self.jdr.server,idchan=self.jdr.channel,name=name_)
+        db.close()
+        self.name = name_
+
+    def setespece(self,espece):
+        db = Database()
+        db.call("petsetespece",dbkey=self.key,charact=self.charkey,idserv=self.jdr.server,idchan=self.jdr.channel,esp=espece)
+        db.close()
+        self.espece = espece
+
+    def switchmod(self,default=False):
+        db = Database()
+        db.call("petswitchmod",dbkey=self.key,charact=self.charkey,idserv=self.jdr.server,idchan=self.jdr.channel,def_=default)
+        db.close()
+        return self.jdr.get_character(self.charkey)
+
+    def lvlup(self):
+        db = Database()
+        db.call("petlevelup",dbkey=self.key,charact=self.charkey,idserv=self.jdr.server,idchan=self.jdr.channel)
+        db.close()
+        self.lvl += 1
+
+    @asyncio.coroutine
+    def roll(self,client,channel,lang,stat,modifier):
+        pass

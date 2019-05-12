cc83daebdf025d3132c47d11f879e7eab98683b6
ttgc <ttgc@users.noreply.github.com>
2019-05-12 23:06:00 +0200

    changed skillconverter to return only list

--- src/discordConverters.py
+++ src/discordConverters.py
@@ -35,7 +35,7 @@ class RaceConverter(commands.Converter):
 class SkillConverter(commands.Converter):
     async def convert(self,ctx,arg):
         if arg.isdecimal():
-            return Skill(int(arg))
+            return [Skill(int(arg))]
         return Skill.skillsearch(arg.replace("_"," "))
 
 class OperatorConverter(commands.Converter):

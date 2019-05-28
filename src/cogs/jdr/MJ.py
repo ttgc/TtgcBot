1f4d2ef81394b926d26f772ff2e98a2d3d000cb9
ttgc <ttgc@users.noreply.github.com>
2019-05-28 19:10:25 +0200

    Add help and logging for MJ commands

--- src/cogs/jdr/MJ.py
+++ src/cogs/jdr/MJ.py
@@ -38,7 +38,10 @@ class MJ(commands.Cog):
 
     @mj.command(name="info",aliases=["charinfo","characterinfo"])
     async def mj_info(self,ctx,char: CharacterConverter):
+        """**GM/MJ only**
+        GM version of character info command"""
         data = GenericCommandParameters(ctx)
+        self.logger.log(logging.DEBUG+1,"/mjcharinfo (%s) in channel %d of server %d",data.char.key,ctx.message.channel.id,ctx.message.guild.id)
         await self.charcog._charinfo(ctx,data,char)
 
     # @mj.command(name="inventory",aliases=["inv"])
@@ -47,26 +50,40 @@ class MJ(commands.Cog):
 
     @mj.command(name="switchmod",aliases=["switchmode"])
     async def mj_switchmod(self,ctx,char: CharacterConverter):
+        """**GM/MJ only**
+        GM version of character switchmod command"""
         data = GenericCommandParameters(ctx)
+        self.logger.log(logging.DEBUG+1,"/mjswitchmod (%s) in channel %d of server %d",data.char.key,ctx.message.channel.id,ctx.message.guild.id)
         await self.charcog._switchmod(ctx,data,char)
 
     @mj.command(name="pay")
     async def mj_pay(self,ctx,char: CharacterConverter,amount: int):
+        """**GM/MJ only**
+        GM version of character pay command"""
         data = GenericCommandParameters(ctx)
+        self.logger.log(logging.DEBUG+1,"/mjpay (%s) in channel %d of server %d",data.char.key,ctx.message.channel.id,ctx.message.guild.id)
         await self.charcog._pay(ctx,data,char,val)
 
     @mj.command(name="setmental")
     async def mj_setmental(self,ctx,char: CharacterConverter,op: typing.Optional[OperatorConverter],amount: int):
+        """**GM/MJ only**
+        GM version of character setmental command"""
         data = GenericCommandParameters(ctx)
+        self.logger.log(logging.DEBUG+1,"/mjsetmental (%s) in channel %d of server %d",data.char.key,ctx.message.channel.id,ctx.message.guild.id)
         await self.charcog._setmental(ctx,data,char,op,val)
 
     @mj.command(name="roll",aliases=["r"])
     async def mj_roll(self,ctx,char,stat,operator: typing.Optional[OperatorConverter] = "+",*,expression=None):
+        """**GM/MJ only**
+        GM version of character roll command"""
         data = GenericCommandParameters(ctx)
+        self.logger.log(logging.DEBUG+1,"/mjroll (%s) in channel %d of server %d",data.char.key,ctx.message.channel.id,ctx.message.guild.id)
         await self.charcog._charroll(ctx,data,char,stat,operator,expression)
 
     @mj.command(name="transfer")
     async def mj_transfer(self,ctx,newMJ: discord.Member):
+        """**GM/MJ only**
+        Transfer ownership of a RP/JDR to someone else (this member needs the GM/MJ role)"""
         data = GenericCommandParameters(ctx)
         destisMJ = discord.utils.get(ctx.message.guild.roles,id=int(data.srv.mjrole)) in newMJ
         if not destisMJ:
@@ -87,6 +104,7 @@ class MJ(commands.Cog):
                     await ctx.message.channel.send(data.lang["mjtransfer_timeout"].format(newMJ))
                 else:
                     data.jdr.MJtransfer(str(newMJ.id))
+                    self.logger.log(logging.DEBUG+1,"/mjtranfer (%d -> %d) in channel %d of server %d",ctx.message.author.id,newMJ.id,ctx.message.channel.id,ctx.message.guild.id)
                     await ctx.message.channel.send(data.lang["mjtransfer"].format(newMJ))
 
     @mj.group(name="pet",invoke_without_command=False)
@@ -94,15 +112,24 @@ class MJ(commands.Cog):
 
     @mj_pet.command(name="roll",aliases=["r"])
     async def mj_pet_roll(self,ctx,char: CharacterConverter,petkey,stat,operator: typing.Optional[OperatorConverter] = "+",*,expression=None):
+        """**GM/MJ only**
+        GM version of pet roll command"""
         data = GenericCommandParameters(ctx)
+        self.logger.log(logging.DEBUG+1,"/mjpetroll (%s / %s) in channel %d of server %d",data.char.key,petkey,ctx.message.channel.id,ctx.message.guild.id)
         await self.petcog._petroll(ctx,data,char,petkey,stat,operator,expression)
 
     @mj_pet.command(name="switchmod",aliases=["switchmode"])
     async def mj_pet_switchmod(self,ctx,char: CharacterConverter,petkey):
+        """**GM/MJ only**
+        GM version of pet switchmod command"""
         data = GenericCommandParameters(ctx)
+        self.logger.log(logging.DEBUG+1,"/mjpetswitchmod (%s / %s) in channel %d of server %d",data.char.key,petkey,ctx.message.channel.id,ctx.message.guild.id)
         await self.petcog._switchmod(ctx,data,char,petkey)
 
     @mj_pet.command(name="info")
     async def mj_pet_info(self,ctx,char: CharacterConverter,petkey):
+        """**GM/MJ only**
+        GM version of pet info command"""
         data = GenericCommandParameters(ctx)
+        self.logger.log(logging.DEBUG+1,"/mjpetinfo (%s / %s) in channel %d of server %d",data.char.key,petkey,ctx.message.channel.id,ctx.message.guild.id)
         await self.petcog._petinfo(ctx,data,char,petkey)

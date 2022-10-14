1fd69e21b89dc4604b810244dade5c666bb2ac88
ttgc <ttgc@users.noreply.github.com>
2022-10-13 21:58:13 -0400

    Fixed deprecated decorator

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -41,7 +41,7 @@ def call_once(raise_error=False):
 
 def deprecated(reason, *, raise_error=True, logger=None):
     def deprecated_decorator(fct):
-        logMethod = logger if logger else _InternalLoggerStorage().logger
+        logMethod = logger if logger else InternalDecoratorLoggerStorage().logger
         logMethod(f"Deprecated function/class: {fct}\nReason: {reason}")
 
         def deprecated_call(*args, **kwargs):

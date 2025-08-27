78528f5b7979ffa5fc5e5ad93c0bffc50eb2f95b
ttgc <ttgc@users.noreply.github.com>
2025-08-27 18:05:36 +0200

    Added LayoutView

--- src/dpylib/ui/components/__init__.py
+++ src/dpylib/ui/components/__init__.py
@@ -17,7 +17,7 @@
 ##    You should have received a copy of the GNU General Public License
 ##    along with this program. If not, see <http://www.gnu.org/licenses/>
 
-from .view import View
+from .view import View, LayoutView
 from .button import Button, button
 from .modal import Modal, modal
 from .textinput import TextInput
